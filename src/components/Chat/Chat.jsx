d1a05d00ac51c54c9de961214360f8e73a80e946
Sumit kumar <sumitkumar@Sumits-MacBook-Air.local>
2021-01-29 17:29:51 +0530

    Server online url added

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -9,7 +9,7 @@ import Input from "../Input/Input";
 import Infobar from "../InfoBar/Infobar";
 import Messages from "../Messages/Messages";
 
-const ENDPOINT = 'http://localhost:5000';
+const ENDPOINT = 'https://chat-room-void.herokuapp.com/';
 
 let socket;
 
